4f15d321bdfe1e3845ccf9ec87a33b12c5813385
Lucas <85549951+LuckyTheCookie@users.noreply.github.com>
2026-01-20 19:01:50 +0100

    fix path

--- app.plugin.js
+++ app.plugin.js
@@ -1,3 +1,3 @@
 // FOSS: Adjusted path for root-level plugin file
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-module.exports = require('./package/lib/commonjs/expo-plugin/withVisionCamera')
+module.exports = require('./lib/commonjs/expo-plugin/withVisionCamera');
